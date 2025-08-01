f1c4dfe1cde093614bea1916287a249ee40a3852
Ajin S <93309006+Ajinstellus@users.noreply.github.com>
2025-08-02 04:42:05 +0530

    Update ec2_install_apache.sh

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
-# shellcheck disable=SC2164
 cd /home/ubuntu
-yes | sudo apt update
-yes | sudo apt install python3 python3-pip
+sudo apt update -y
+sudo apt install -y python3 python3-pip git
+
 git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
-sleep 20
-# shellcheck disable=SC2164
 cd python-mysql-db-proj-1
 pip3 install -r requirements.txt
-echo 'Waiting for 30 seconds before running the app.py'
-setsid python3 -u app.py &
-sleep 30
+
+# Wait for DB to be ready if needed
+sleep 10
+
+# Start the app and log output
+nohup python3 app.py > flask.log 2>&1 &
